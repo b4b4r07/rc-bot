f30a9ad033de6f1ecf3429c782e9a4b07d7488ab
b4b4r07 <b4b4r07@gmail.com>
2016-12-31 18:46:48 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+
+	"github.com/nlopes/slack"
+)
+
+var pattern *regexp.Regexp = regexp.MustCompile(`^bot\s+rc\s+(.*)`)
+
+var (
+	shell = flag.String("s", os.Getenv("SHELL"), "Specify the shell")
+)
+
+func main() {
+	flag.Parse()
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	os.Exit(run(api))
+}
+
+func run(api *slack.Client) int {
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	var params slack.PostMessageParameters
+
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			switch ev := msg.Data.(type) {
+			case *slack.HelloEvent:
+				log.Print("Connected!")
+
+			case *slack.MessageEvent:
+				if len(ev.Text) > 0 {
+					pat := pattern.FindStringSubmatch(ev.Text)
+					if len(pat) == 0 {
+						params = getPostMessageParameters("too few arguments", false)
+					} else {
+						result, err := runCommand(pat[1])
+						if err == nil {
+							params = getPostMessageParameters(result, true)
+						} else {
+							params = getPostMessageParameters(err.Error(), false)
+						}
+					}
+					_, _, err := api.PostMessage(ev.Channel, "", params)
+					if err != nil {
+						log.Print(err)
+						return 1
+					}
+				}
+
+			case *slack.InvalidAuthEvent:
+				log.Print("Invalid credentials")
+				return 1
+			}
+		}
+	}
+}
+
+func runCommand(cmd string) (string, error) {
+	out, err := exec.Command(*shell, "-c", cmd).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func getPostMessageParameters(result string, ok bool) slack.PostMessageParameters {
+	color := "danger"
+	if ok {
+		color = "good"
+	}
+
+	params := slack.PostMessageParameters{
+		Markdown:  true,
+		Username:  "rc-bot",
+		IconEmoji: ":trollface:",
+	}
+	params.Attachments = []slack.Attachment{}
+	params.Attachments = append(params.Attachments, slack.Attachment{
+		Fallback:   "",
+		Title:      "",
+		Text:       result,
+		MarkdownIn: []string{"title", "text", "fields", "fallback"},
+		Color:      color,
+	})
+	return params
+}
